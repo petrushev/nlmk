0eb0b9b7d9146175ebf41b4d1ed2309875a6047d
Baze Petrushev <b.petrushev@gmail.com>
2012-01-16 19:49:51 +0100

    Test: text.py modified

--- test/text.py
+++ test/text.py
@@ -8,11 +8,13 @@ from nlmk import stopwords, ra_unicode_read
 stopwords = stopwords()
 
 from nlmk.text import sentence, iter_sentences, iter_tokens,\
-                      iter_ngrams, collocations, frequency, concordance
+                      iter_ngrams, collocations, frequency, concordance,\
+                      vocabulary, contexts, similar
+                      
 from codecs import open
 
-#fh = open("racin.txt", 'r', 'utf-8')
-fh = open("nebeska.txt", 'r', 'utf-8')
+fh = open("racin.txt", 'r', 'utf-8')
+#fh = open("nebeska.txt", 'r', 'utf-8')
 content = fh.read()
 
 fh.seek(0)
@@ -21,19 +23,26 @@ sent_idx = sentences_index(content)
 del content
 
 isents = iter_sentences(fh, sent_idx)
-
-itokens = (t for t, s, tid in iter_tokens(isents))
+#for i in isents: print i
+itokens = iter(t for t, s, tid in iter_tokens(isents))
 
 
 ibig = iter_ngrams(itokens, n=2) 
-#for s in    stopwords():
+#for s in stopwords():
 #    print s
 
 col = collocations(ibig)
 for l, r in col:
     print l,r
 
+#vocab = vocabulary(iter_tokens(isents))
+#print vocab
 
+for l,r in contexts(u'не', itokens):
+    print l, u'не', r 
+ 
+#for s in similar(u'свет', itokens):
+#    print s
 
 #t0=time()
 #fq =  frequency(itokens)

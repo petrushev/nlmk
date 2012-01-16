b964f0cfd968f7880a2aa191c57744f1b205634c
Baze Petrushev <b.petrushev@gmail.com>
2012-01-16 17:28:49 +0100

    Test: text changed

--- test/text.py
+++ test/text.py
@@ -8,13 +8,14 @@ from nlmk import stopwords, ra_unicode_read
 stopwords = stopwords()
 
 from nlmk.text import sentence, iter_sentences, iter_tokens,\
-                      iter_ngrams, collocations, frequency, concordance
+                      iter_ngrams, collocations, frequency, concordance,\
+                      default_collocation_filter
 from codecs import open
 
 #fh = open("racin.txt", 'r', 'utf-8')
-fh = open("nebeska.txt", 'r', 'utf-8')
+#fh = open("nebeska.txt", 'r', 'utf-8')
+fh = open("lek_protiv_melanholijata.txt", 'r', 'utf-8')
 content = fh.read()
-
 fh.seek(0)
 
 sent_idx = sentences_index(content)
@@ -24,24 +25,22 @@ isents = iter_sentences(fh, sent_idx)
 
 itokens = (t for t, s, tid in iter_tokens(isents))
 
-
 ibig = iter_ngrams(itokens, n=2) 
-#for s in    stopwords():
-#    print s
 
-col = collocations(ibig)
-for l, r in col:
-    print l,r
+def alpha_col_filter(word):
+    return word > u'a' and word< u'џџ' and default_collocation_filter(word) 
+
+#col = collocations(ibig, filter=alpha_col_filter)
+#for l, r in col:
+#    print l,r
 
 
 
-#t0=time()
-#fq =  frequency(itokens)
-#print time()-t0
+fq =  frequency(itokens)
 
-#fq = ((t, c) for t, c in fq.iteritems() \
-#      if c>4 and t not in stopwords)
-#for t, c in sorted(fq, key=lambda item:-1*item[1]):
-#    print t,c
+fq = ((t, c) for t, c in fq.iteritems() \
+      if c>4)
+for t, c in sorted(fq, key=lambda item:-1*item[1]):
+    print t
 
 fh.close()

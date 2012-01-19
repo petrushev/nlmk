4cce692d1e00f82579d041687165e43a4ffc1790
Baze Petrushev <b.petrushev@gmail.com>
2012-01-19 18:37:13 +0100

    Tagger modified [buggy]

--- test/text.py
+++ test/text.py
@@ -2,6 +2,8 @@
 
 from StringIO import StringIO
 from time import time
+from itertools import tee
+from pprint import pprint
 
 from nlmk.tokenizer import tokenize, sentences_index
 from nlmk import stopwords, ra_unicode_read
@@ -11,7 +13,7 @@ from nlmk.text import sentence, iter_sentences, iter_tokens,\
                       iter_ngrams, collocations, frequency, concordance,\
                       default_collocation_filter
 
-from nlmk.tagger import tag
+from nlmk.tagger import base_tag, tag
 
 from codecs import open
 
@@ -49,25 +51,53 @@ def alpha_col_filter(word):
 #content = ra_unicode_read(fh, 0, 15000)
 
 tokens = tokenize(content)
+del content
+fh.close()
+
+itagged1 = ( (token, base_tag(tag(token))) for token in tokens)
+
+itagged1, itagged2 = tee(itagged1)
 
 all_ = 0
 tagged = {}
-for t in tokens:
+for token, tag_ in itagged1:
     all_=all_+1
-    tag_=tag(t)
     if tag_ :
         tagged[tag_]=tagged.get(tag_,0)+1
 #    if t.endswith(u'ам'):
 #        print t
 
-for l,c,r in iter_ngrams(tokens,3):
-    ltag, ctag, rtag = tag(l), tag(c), tag(r)
+#llx ={}
+lxr ={}
+
+tagged_trigrams = list(iter_ngrams(itagged2,3))
+
+for l,c,r in tagged_trigrams:
+    (l, ltag), (c, ctag), (r, rtag)=l,c,r
     if ltag and ltag!='PU' and rtag and rtag!='PU'\
        and ctag and ctag!='PU':
-        print l,c,r,'',ltag,ctag,rtag  
-
-
+        #print l,c,r,'',ltag,ctag,rtag
+        #try:
+        #    llx[(ltag,ctag)].add(rtag)
+        #except KeyError:
+        #    llx[(ltag,ctag)]=set([rtag])
+        try:
+            lxr[(ltag,rtag)].add(ctag)
+        except KeyError:
+            lxr[(ltag,rtag)]=set([ctag])
+              
 print tagged
 print sum(tagged.itervalues())*100/all_
 
-fh.close()
+valid = [ (l, list(set_)[0], r)  \
+          for (l, r), set_  in lxr.iteritems() \
+          if len(set_)==1 ]
+
+for (l, ltag), (c,ctag), (r,rtag) in tagged_trigrams:
+    if ltag and rtag and ltag!='PU' and rtag!='PU' and ctag is None:
+        for ltag_, ctag_, rtag_ in valid:
+            if ltag==ltag_ and rtag==rtag_:
+                pass
+                #print c, ctag_
+
+

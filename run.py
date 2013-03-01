391a1c6dc16a943488a6598d567120d1f4fe7bbd
Baze Petrushev <b.petrushev@gmail.com>
2013-03-01 15:10:50 +0100

    Added information:     - readme (added example)     - run.py: available runners

--- run.py
+++ run.py
@@ -1,14 +1,13 @@
-
 from sys import argv
 from nlmk import ngramgen as ngramgenmod
 
-import codecs 
+import codecs
 from nlmk import text
 from nlmk import tokenizer
 
-
 def ngramgen(source, target, *cuttoff_info):
-    
+    """Generate n-grams with provided cuttoff"""
+
     try:
         fh = codecs.open(source, 'r', 'utf-8')
     except Exception:
@@ -20,18 +19,18 @@ def ngramgen(source, target, *cuttoff_info):
     except Exception:
         raise
         return
-    
+
     try:
         cuttoff_info = map(int, cuttoff_info)
     except ValueError:
         print 'Invalid cuttoff info provided, list of integers needed'
         return
-    
-    if len(cuttoff_info)==0:
+
+    if len(cuttoff_info) == 0:
         print 'Cuttoff info provided is zero length'
         return
 
-    
+
     content = fh.read()
 
     fh.seek(0)
@@ -44,15 +43,17 @@ def ngramgen(source, target, *cuttoff_info):
     itokens = (t for t, s, tid in text.iter_tokens(isents))
     res = ngramgenmod.multi_ngram(itokens, len(cuttoff_info))
     fh.close()
-    
+
     res = ngramgenmod.cutt_ngrams(res, cuttoff_info)
 
     for dict_ in res:
         for tpl, v in dict_.iteritems():
-            fh2.write( ' '.join(tpl)+' '+str( v) + '\n')
+            fh2.write(' '.join(tpl) + ' ' + str(v) + '\n')
 
     fh2.close()
 
+
+_runners = {'ngramgen': ngramgen}
 
 def main():
     try:
@@ -61,17 +62,20 @@ def main():
         print('No runner provided.')
         return
 
-    if case=='main': return    
+    if case == 'main': return
+
     try:
-        case = globals()[case]
+        case = _runners[case]
     except KeyError:
-        print('Runner %s not found.' % case)
+        print 'Runner %s not found.' % case
+        print 'Available runners:'
+        for runner, runner_fc in _runners.iteritems():
+            print '    %s: %s' % (runner, runner_fc.__doc__)
         return
-    
-    
+
     args = argv[2:]
     case(*args)
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()

c3f3eca50361a86853a0aa7e9d2e0a38e8e75d72
petrushev <b.petrushev@gmail.com>
2020-05-01 20:35:22 +0200

    Bugfix: end-of-file handling

--- nlmk/__init__.py
+++ nlmk/__init__.py
@@ -3,31 +3,42 @@ from operator import add
 
 def ra_unicode_read(fh, start, end):
     """Read random-access file on positions given as unicode string"""
-
     tmp_pos = fh.tell()
     fh.seek(0)
 
     buf = u""
-    iter_fh = iter(fh)
-    iter_fh = (line.decode('utf-8') for line in iter_fh)
-    for line in iter_fh:  # read file line-by-line...
+    iter_fh = (line.decode('utf-8') for line in fh)
+
+    for line in iter_fh:
         line_len = len(line)
-        if line_len < start:        # .. recalibrate start/end ...
+        # .. recalibrate start/end ...
+        if line_len < start:
             start = start - line_len
             if end is not None:
                 end = end - line_len
-        else:                     # ...until start is met
+        else:
+            # ...until start is met
             buf = buf + line
-            buf = buf[start:]       # cutoff start
+
+            # cutoff start
+            buf = buf[start:]
             if end is not None:
                 end = end - start
+
             break
+
     if end is not None:
-        while len(buf) < end:             # read enough lines so the end is met
-            buf = buf + iter_fh.next()
+        # read enough lines so the end is met
+        while len(buf) < end:
+            try:
+                buf = buf + iter_fh.next()
+            except StopIteration:
+                # end-of-file: end will not be met
+                break
         buf = buf[:end]
 
     fh.seek(tmp_pos)   # restore the file position
+
     return buf
 
 
